6e985b2b3fb5241f4c9ab84f781d689c54035f3c
Riddhi Jain <rjain_be24@thapar.edu>
2026-02-24 22:22:49 +0530

    more visuals added

--- src/train_model.py
+++ src/train_model.py
@@ -8,37 +8,6 @@ from sklearn.linear_model import LogisticRegression
 
 from data_preprocessing import load_data, preprocess_data
 
-# # -----------------------------
-# # 1. Load Data
-# # -----------------------------
-# def load_data(path):
-#     df = pd.read_csv(path)
-#     return df
-
-
-# # -----------------------------
-# # 2. Preprocess Data
-# # -----------------------------
-# def preprocess_data(df):
-#     # Fix TotalCharges datatype
-#     df["TotalCharges"] = pd.to_numeric(df["TotalCharges"], errors="coerce")
-#     df["TotalCharges"] = df["TotalCharges"].fillna(df["TotalCharges"].median())
-
-#     # Separate target
-#     X = df.drop("Churn", axis=1)
-#     y = df["Churn"]
-
-#     # Drop ID column
-#     X = X.drop("customerID", axis=1)
-
-#     # One-hot encoding
-#     X = pd.get_dummies(X, drop_first=True)
-
-#     # Encode target
-#     y = y.map({"No": 0, "Yes": 1})
-
-#     return X, y
-
 
 # -----------------------------
 # 3. Train Model
